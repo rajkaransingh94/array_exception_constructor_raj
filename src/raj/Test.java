c98e4e19701ffd9e3714007f88bbedaa1f07ea0e
rajkaransingh94 <129711888+rajkaransingh94@users.noreply.github.com>
2023-04-03 10:15:37 +0530

    new file added

---
+++ src/raj/Test.java
@@ -0,0 +1,19 @@
+package raj;
+
+public class Test {
+	System.out.println("hi from constructor body");
+	
+	}
+public class Test(int a,int b)
+{
+	int c=a+b;
+	System.out.println(c);
+}
+}
+public class Test{
+	public static void main(String[] args) {
+		new class test(12,13);
+	}
+}
+
+
--- src/raj/exception.java
+++ src/raj/exception.java
@@ -1,5 +1,22 @@
 package raj;
 
-public class exception {
+import java.util.Scanner;
 
+public class exception {
+public static void main(String[] args) {
+	Scanner scanner=new Scanner(System.in);
+	System.out.println("enter your number");
+	int a,b,c;
+	try
+	{
+		a=scanner.nextInt();
+		b=scanner.nextInt();
+		c=a/b;
+		System.out.println(c);
+	}
+	catch (Exception e) {
+		System.out.println("please do proper input");
+		
+	}
+}
 }
